4b41fb6935820f74dfd2aabf3b96eb6410859322
louisaberger <lberger9@gmail.com>
2013-03-28 15:38:38 -0400

    mock files

---
+++ src/unittests/utest1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+	if (argc == 1) {
+		std::cout << "Need to specify true or false on cmdline\n";
+		return 0;
+	}
+	else {
+		std::string flag (argv[1]);
+		if (flag.compare("true") == 0 ) {
+			std::cout << "True \n";
+			return 1;
+		}
+		else {
+			std::cout << "False \n";
+			return 0;
+		}
+	}
+}
