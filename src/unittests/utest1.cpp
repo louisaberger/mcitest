3bca00da6799382d35ffb487f61be8fbc0c920a0
louisaberger <lberger9@gmail.com>
2013-03-28 15:47:48 -0400

    success return 0

--- src/unittests/utest1.cpp
+++ src/unittests/utest1.cpp
@@ -5,17 +5,17 @@ int main(int argc, char* argv[])
 {
 	if (argc == 1) {
 		std::cout << "Need to specify true or false on cmdline\n";
-		return 0;
+		return 2;
 	}
 	else {
 		std::string flag (argv[1]);
 		if (flag.compare("true") == 0 ) {
 			std::cout << "True \n";
-			return 1;
+			return 0;
 		}
 		else {
 			std::cout << "False \n";
-			return 0;
+			return 1;
 		}
 	}
 }
